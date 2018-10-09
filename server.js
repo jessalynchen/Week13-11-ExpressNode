d89cc41e841937905135e610a7ecd3310829ac34
jessalynchen <jessalynchen@gmail.com>
2018-10-08 23:20:24 -0400

    change portlocation

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8008;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -11,8 +11,12 @@ app.use(bodyParser.json());
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-app.listen(PORT, function() {
-  console.log("Listen on Port " + PORT);
+app.listen(PORT, function(error) {
+  if (error){
+    console.log(error);
+  } else {
+    console.log("Listen on Port " + PORT);
+  }
 });
 
 //remember to npm i express
